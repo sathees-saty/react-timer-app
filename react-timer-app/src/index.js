0d14bdfc973f39f7f04f209cf2eaa9a896ca5f4f
Sathees Kumar K <65384711+sathees-saty@users.noreply.github.com>
2024-04-03 21:56:46 +0530

    Add files via upload

---
+++ react-timer-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+function tick() {
+  const element = (
+    <div style={{ backgroundColor: 'lightblue' }}>
+      <h1>Timer App</h1>
+      <hr></hr>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(tick, 1000);
